3f7db6c85cf107b9b26ef5ecd6e184ceb3ccdb67
Luiz Felipe <felipe.silva337@yahoo.com>
2024-08-23 23:01:50 -0300

    fix: update algoritmo10.c

--- src/algoritmo10.c
+++ src/algoritmo10.c
@@ -7,29 +7,29 @@
 int main(void)
 {
     float note1;
-    int note1_weight;
+    float note1_weight;
     float note2;
-    int note2_weight;
+    float note2_weight;
     float note3;
-    int note3_weight;
+    float note3_weight;
 
     printf("Insira a nota 1: ");
     scanf("%f", &note1);
     printf("Insira o peso da nota 1: ");
-    scanf("%d", &note1_weight);
+    scanf("%f", &note1_weight);
 
     printf("Insira a nota 2: ");
     scanf("%f", &note2);
     printf("Insira o peso da nota 2: ");
-    scanf("%d", &note2_weight);
+    scanf("%f", &note2_weight);
 
     printf("Insira a nota 3: ");
     scanf("%f", &note3);
     printf("Insira o peso da nota 3: ");
-    scanf("%d", &note3_weight);
+    scanf("%f", &note3_weight);
 
-    float sum = (note1 * note1_weight) + (note2 * note2_weight) + (note3_weight * note3_weight);
-    int total_weights = note1_weight + note2_weight + note3_weight;
+    float sum = (note1 * note1_weight) + (note2 * note2_weight) + (note3 * note3_weight);
+    float total_weights = note1_weight + note2_weight + note3_weight;
 
     puts("----------------");
     printf("Media ponderada: %.2f\n", sum / total_weights);
